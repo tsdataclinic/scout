c08513bcf00da0f6b19d9c54bedf220a9a4e4a97
Stuart Lynn <stuart.lynn@gmail.com>
2020-01-07 17:36:54 -0500

    Hooks for accessing and filtering the datasets

--- src/hooks/datasets.js
+++ src/hooks/datasets.js
@@ -14,19 +14,32 @@ export function useCategories() {
 export function useDatasets({tags, term}) {
   let [{datasets}] = useStateValue();
   return useMemo(() => {
-    if (tags) {
-      datasets = datasets.filter(dataset =>
+    let filteredDatasets = [...datasets];
+    console.log(
+      'Filtering ',
+      filteredDatasets.length,
+      ' with tags ',
+      tags,
+      ' and search term ',
+      term,
+    );
+    if (tags && tags.length > 0) {
+      console.log('applting');
+      filteredDatasets = filteredDatasets.filter(dataset =>
         dataset.classification.domain_tags.includes(tags[0]),
       );
     }
 
-    if (term) {
-      datasets = datasets.filter(dataset =>
+    console.log('after tags ', filteredDatasets.length);
+
+    if (term && term.length > 0) {
+      filteredDatasets = filteredDatasets.filter(dataset =>
         dataset.resource.name.toLowerCase().includes(term.toLowerCase()),
       );
     }
 
-    console.log('return size ', datasets.length);
-    return datasets;
+    console.log('after term ', filteredDatasets.length);
+    console.log('return size ', filteredDatasets.length);
+    return filteredDatasets;
   }, [tags, term, datasets]);
 }
