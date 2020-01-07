52f4a6ccd98477483f4e9497069f729d7173f0e4
Stuart Lynn <stuart.lynn@gmail.com>
2020-01-07 17:37:32 -0500

    Adding some quick ways for view the results of column matching

--- src/layout/HomePage.js
+++ src/layout/HomePage.js
@@ -1,57 +1,67 @@
 import React, {useState} from 'react';
 import {useCategories, useTags, useDatasets} from '../hooks/datasets';
+import {findJoinable} from '../utils/socrata';
+import CategorySelector from '../components/CategorySelector';
+import Dataset from '../components/Dataset';
+import usePagination from '../hooks/pagination';
 
 export default function HomePage({}) {
   const categories = useCategories();
   const tags = useTags();
-  const [tag, setTag] = useState('buildings');
+  const [tag, setTag] = useState(['buildings']);
   const [searchTerm, setSearchTerm] = useState('');
-  const datasets = useDatasets({tags: [tag], term: searchTerm});
-  console.log('outside dataset size ', datasets.length);
-  console.log('filtered datasets are ', datasets);
+  const datasets = useDatasets({tags: tag, term: searchTerm});
+  const [pagedDatasets, {pageButtons}] = usePagination(datasets);
+
+  const clearTag = () => {
+    console.log('Clearning tab');
+    setTag([]);
+  };
+
   return (
     <div className="HomePage">
       <h1>Home Page</h1>
-      <h2>Tags: {tag}</h2>
+      <h2>Categories</h2>
+      <CategorySelector categories={categories} />
+      <h2>
+        Tags: {tag} <span onClick={clearTag}>X</span>
+      </h2>
       {tags && (
         <ul className="tag-list">
           {tags.map(tag => (
-            <li onClick={() => setTag(tag)} key={tag}>
+            <li onClick={() => setTag([tag])} key={tag}>
               {tag}
             </li>
           ))}
         </ul>
       )}
       <h2>Datasets</h2>
+      {pageButtons}
       <input
         type="text"
         onChange={e => setSearchTerm(e.target.value)}
         value={searchTerm}
         placeholder="search"
       />
-      {datasets && (
-        <ul style={{height: '300px', overflowY: 'auto'}}>
-          {datasets.map((dataset, index) => (
-            <li key={index}>
-              <h4>
-                <a target="_blank" href={dataset.permalink}>
-                  {dataset.resource.name}
-                </a>
-              </h4>
-              <p>{dataset.resource.description}</p>
-              <p>{dataset.classification.domain_tags.join(', ')}</p>
-            </li>
-          ))}
-        </ul>
-      )}
-      <h2>Categories</h2>
-      {categories && (
-        <ul>
-          {categories.map(category => (
-            <li key={category}>{category}</li>
-          ))}
-        </ul>
-      )}
+      <ul style={{height: '300px', overflowY: 'auto'}}>
+        {pagedDatasets.map(dataset => (
+          <li>
+            <Dataset dataset={dataset} />
+            <h2>Potential Matches</h2>
+            <ul>
+              {findJoinable(dataset, datasets).map(match => (
+                <li>
+                  <span style={{fontWeight: 'bold'}}>
+                    {' '}
+                    {match.dataset.resource.name}
+                  </span>{' '}
+                  | shares {match.joinableColumns.join(', ')}{' '}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
