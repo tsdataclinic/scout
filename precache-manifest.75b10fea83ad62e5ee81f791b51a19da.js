187eb81713c9847f992c2b8dd2262794eca2d4ee
Stuart Lynn <stuart.lynn@gmail.com>
2020-01-14 11:32:07 -0500

    Updates

---
+++ precache-manifest.75b10fea83ad62e5ee81f791b51a19da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f885d49f56ed24275719b79801af893",
+    "url": "/NewYorkOpenDataExplorer/index.html"
+  },
+  {
+    "revision": "a5bb5d405b9ec84ee8e1",
+    "url": "/NewYorkOpenDataExplorer/static/css/main.1e03da4b.chunk.css"
+  },
+  {
+    "revision": "244a8438f0f9a4033685",
+    "url": "/NewYorkOpenDataExplorer/static/js/2.2f5523ba.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/NewYorkOpenDataExplorer/static/js/2.2f5523ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5bb5d405b9ec84ee8e1",
+    "url": "/NewYorkOpenDataExplorer/static/js/main.b2f8b8c9.chunk.js"
+  },
+  {
+    "revision": "e8f84f0c19fdd1dc0119",
+    "url": "/NewYorkOpenDataExplorer/static/js/runtime-main.ee9bcc08.js"
+  }
+]);
