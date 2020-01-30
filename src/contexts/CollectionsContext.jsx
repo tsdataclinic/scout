a435b2a6433cf1f73adef25625f6ba2f7ebfe867
Stuart Lynn <stuart.lynn@gmail.com>
2020-01-30 09:52:10 -0500

    Make collections persistent over refreshes

--- src/contexts/CollectionsContext.jsx
+++ src/contexts/CollectionsContext.jsx
@@ -1,11 +1,18 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import React, { createContext, useContext, useReducer, useEffect } from 'react';
+import Dexie from 'dexie';
 
 export const CollectionsContext = createContext();
 
 const initalState = {
   datasets: [],
   name: null,
+  cacheLoaded: false,
 };
+
+const db = new Dexie('CollectionCache');
+db.version(1).stores({
+  CollectionCache: 'id',
+});
 
 const reducer = (state, action) => {
   const { type, payload } = action;
@@ -27,6 +34,11 @@ const reducer = (state, action) => {
         ...state,
         datasets: [],
       };
+    case 'HYDRATE_STATE':
+      return {
+        ...state,
+        ...payload,
+      };
     default:
       return state;
   }
@@ -34,6 +46,38 @@ const reducer = (state, action) => {
 
 export const CollectionsProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initalState);
+  const { cacheLoaded, datasets, name } = state;
+
+  // Restore state
+  useEffect(() => {
+    db.CollectionCache.get(1).then((result) => {
+      if (result) {
+        const cachedState = JSON.parse(result.data);
+        dispatch({
+          type: 'HYDRATE_STATE',
+          payload: { ...initalState, ...cachedState, cacheLoaded: true },
+        });
+      } else {
+        dispatch({
+          payload: { ...initalState, cacheLoaded: true },
+          type: 'HYDRATE_STATE',
+        });
+      }
+    });
+  }, []);
+
+  // Cache state
+  useEffect(() => {
+    if (cacheLoaded) {
+      db.CollectionCache.put({
+        data: JSON.stringify({
+          datasets,
+          name,
+        }),
+        id: 1,
+      });
+    }
+  }, [cacheLoaded, datasets, name]);
 
   return (
     <CollectionsContext.Provider value={[state, dispatch]}>
