d475925767ba4e6395fccd2d06e4e8e7e40f4434
Stuart Lynn <stuart.lynn@gmail.com>
2020-02-05 13:37:42 -0500

    Formatter for hilighting text based on fuzzy search results

--- src/utils/formatters.js
+++
@@ -1,8 +0,0 @@
-export function formatDate(date) {
-  const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  };
-  return new Date(date).toLocaleDateString('en-US', options);
-}
