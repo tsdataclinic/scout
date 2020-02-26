4a6eb7c1d4db2cda6586538cbcd4c932b9c12734
Stuart Lynn <stuart.lynn@gmail.com>
2020-02-26 15:09:04 -0500

    bug fixes after merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.scss';
 import {
-    BrowserRouter as Router,
-    Route,
-    Redirect,
-    Switch,
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
 } from 'react-router-dom';
 import { ModalContainer, ModalRoute } from 'react-router-modal';
 
@@ -35,6 +35,7 @@ function App() {
               component={CollectionPage}
             />
 
+            <Route path="/about" component={AboutPage} />
             <ModalRoute
               path="/collection/new"
               parentPath="/"
@@ -44,13 +45,12 @@ function App() {
             <Redirect from="/" to="/" />
           </Switch>
         </div>
-          <GHPagesRedirect />
-          <WelcomeRedirect />
-          <ModalRoute path="/welcome" parentPath="/" component={WelcomeModal} />
-          <CollectionBar />
-        </Router>
-     </div>
-    );
+        <GHPagesRedirect />
+        <WelcomeRedirect />
+        <ModalRoute path="/welcome" parentPath="/" component={WelcomeModal} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
