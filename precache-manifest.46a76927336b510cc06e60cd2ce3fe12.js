b528ea5cbec4d9609ed09b756dc75dddf9f2435b
Stuart Lynn <stuart.lynn@gmail.com>
2020-02-26 17:00:35 -0500

    Updates

---
+++ precache-manifest.46a76927336b510cc06e60cd2ce3fe12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a170d07d36bb8df7c7e6ab89e04dab8",
+    "url": "/scout/index.html"
+  },
+  {
+    "revision": "082a8d6b91fc20d52c83",
+    "url": "/scout/static/css/2.349b3bfd.chunk.css"
+  },
+  {
+    "revision": "b64889f35968111b2159",
+    "url": "/scout/static/css/main.e623b602.chunk.css"
+  },
+  {
+    "revision": "082a8d6b91fc20d52c83",
+    "url": "/scout/static/js/2.e0e2fa16.chunk.js"
+  },
+  {
+    "revision": "e81ad11c94f7803073e2b75bc0f0b11d",
+    "url": "/scout/static/js/2.e0e2fa16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b64889f35968111b2159",
+    "url": "/scout/static/js/main.951211af.chunk.js"
+  },
+  {
+    "revision": "572c3503f2fb0ce583a4",
+    "url": "/scout/static/js/runtime-main.bff67842.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NewYorkOpenDataExplorer/precache-manifest.bedcec3fd89e982aec2330713ec63171.js"
+  "/scout/precache-manifest.46a76927336b510cc06e60cd2ce3fe12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NewYorkOpenDataExplorer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scout/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
