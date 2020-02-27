b41c065b8cd126a6d0efdd7848c8a270fb73cd79
Stuart Lynn <stuart.lynn@gmail.com>
2020-02-27 09:03:25 -0500

    Loads in the unique id set in bulk. Need to make this not run for datasets with a lot of unique id's

--- src/components/ColumnSuggestions/ColumnSuggestions.jsx
+++ src/components/ColumnSuggestions/ColumnSuggestions.jsx
@@ -7,23 +7,50 @@ import usePagination from '../../hooks/pagination';
 import { getUniqueEntries } from '../../utils/socrata';
 
 export default function ColumnSuggestions({ column, joins, dataset }) {
-  const [columnUniques, setColumnsUniques] = useState([]);
-
   const [collapsed, setCollapsed] = useState(true);
+  const [overlaps, setOverlaps] = useState(null);
+
   const dataTypeForCol =
     dataset.resource.columns_datatype[
       dataset.resource.columns_name.indexOf(column)
     ];
 
-  const [pagedJoins, { pageButtons }] = usePagination(joins, 10);
+  const [pagedJoins, { pageButtons }] = usePagination(
+    overlaps
+      ? overlaps.map((o) => joins.find((j) => j.dataset.resource.id === o.id))
+      : joins,
+    10,
+  );
+
+  if (overlaps) {
+    console.log('overlaps are ', overlaps);
+  }
+  if (overlaps) {
+    console.log('paged joins are ', pagedJoins);
+  }
 
   useEffect(() => {
-    if (joins.length > 0) {
-      getUniqueEntries(dataset, column).then((res) => {
-        setColumnsUniques(res);
-      });
+    if (collapsed === false && overlaps === null) {
+      if (joins.length > 0) {
+        getUniqueEntries(dataset, column).then((parentUniques) => {
+          Promise.all(
+            joins.map((j) =>
+              getUniqueEntries(j.dataset, column).then((res) => ({
+                id: j.dataset.resource.id,
+                matches: parentUniques.filter((e) => res.includes(e)),
+              })),
+            ),
+          ).then((results) =>
+            setOverlaps(
+              results.sort((a, b) =>
+                a.matches.length > b.matches.length ? -1 : 1,
+              ),
+            ),
+          );
+        });
+      }
     }
-  }, [column, dataset, joins]);
+  }, [column, dataset, joins, collapsed, overlaps]);
 
   return (
     <div className={`column-suggestions ${collapsed ? 'collapsed' : ''}`}>
@@ -48,7 +75,7 @@ export default function ColumnSuggestions({ column, joins, dataset }) {
       {!collapsed && pagedJoins && (
         <div className="columns-suggestions-matches">
           <h3>MATCHING DATSETS</h3>
-          {joins && (
+          {joins && overlaps && (
             <ul>
               {pagedJoins.map((join) => (
                 <li key={join.dataset.resource.id}>
@@ -56,7 +83,10 @@ export default function ColumnSuggestions({ column, joins, dataset }) {
                     leftDataset={dataset}
                     rightDataset={join.dataset}
                     joinCol={column}
-                    parentUniques={columnUniques}
+                    matches={
+                      overlaps.find((o) => o.id === join.dataset.resource.id)
+                        .matches
+                    }
                   />
                 </li>
               ))}
--- src/components/JoinColumn/JoinColumn.jsx
+++ src/components/JoinColumn/JoinColumn.jsx
@@ -1,89 +1,77 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFile } from '@fortawesome/free-regular-svg-icons';
 import { Link } from 'react-router-dom';
 import {
-    faAngleRight,
-    faAngleDown,
-    faTable,
+  faAngleRight,
+  faAngleDown,
+  faTable,
 } from '@fortawesome/free-solid-svg-icons';
-import { useUniqueColumnEntries } from '../../hooks/datasets';
 import { useCurrentCollection } from '../../hooks/collections';
 import './JoinColumn.scss';
 
-export default function ColumnJoin({ rightDataset, joinCol, parentUniques }) {
-    const [collapsed, setCollapsed] = useState(true);
-    const uniqueIds = useUniqueColumnEntries(rightDataset, joinCol);
-    const jointlyUnique = useMemo(
-        () =>
-            uniqueIds && parentUniques
-                ? parentUniques.filter((e) => uniqueIds.distinct.includes(e))
-                : [],
-        [uniqueIds, parentUniques],
-    );
-    const [
-        collection,
-        { addToCollection, removeFromCollection },
-    ] = useCurrentCollection();
+export default function ColumnJoin({ rightDataset, matches }) {
+  const [collapsed, setCollapsed] = useState(true);
+  const [
+    collection,
+    { addToCollection, removeFromCollection },
+  ] = useCurrentCollection();
 
-    return (
-        <div className="join-column">
-            <div className="join-column-row">
-                <span
-                    onClick={() => setCollapsed(!collapsed)}
-                    onKeyDown={(e) => {
-                        if (e.keycode === 32) {
-                            setCollapsed(!collapsed);
-                        }
-                    }}
-                    className="join-column-name"
-                    role="button"
-                    tabIndex="0"
-                >
-                    <FontAwesomeIcon
-                        icon={collapsed ? faAngleRight : faAngleDown}
-                    />
-                    <FontAwesomeIcon icon={faFile} />
-                    {rightDataset.resource.name}
-                </span>
-                <span>
-                    {jointlyUnique
-                        ? `${jointlyUnique.length} matching ids`
-                        : 'loading'}
-                </span>
-                <span>
-                    <Link to={`/dataset/${rightDataset.resource.id}`}>
-                        View dataset
-                    </Link>
-                </span>
-                <button
-                    type="button"
-                    onClick={() =>
-                        collection.datasets.includes(rightDataset.resource.id)
-                            ? removeFromCollection(rightDataset.resource.id)
-                            : addToCollection(rightDataset.resource.id)
-                    }
-                >
-                    {collection.datasets.includes(rightDataset.resource.id)
-                        ? 'Remove dataset from collection'
-                        : 'Add dataset to collection'}
-                </button>
-            </div>
-            {!collapsed && uniqueIds && (
-                <div className="join-column-unique">
-                    <h3>COMMON IDS</h3>
-                    <ul>
-                        {jointlyUnique.slice(0, 10).map((uid) => (
-                            <li key={uid}>
-                                <div className="bar" />
-                                <FontAwesomeIcon icon={faTable} />
-                                <span>{uid}</span>
-                            </li>
-                        ))}
-                    </ul>
-                    <div>and {jointlyUnique.length - 10} more</div>
-                </div>
-            )}
-        </div>
-    );
+  console.log('matches are ', matches);
+
+  return (
+    <div className="join-column">
+      <div className="join-column-row">
+        <span
+          onClick={() => setCollapsed(!collapsed)}
+          onKeyDown={(e) => {
+            if (e.keycode === 32) {
+              setCollapsed(!collapsed);
+            }
+          }}
+          className="join-column-name"
+          role="button"
+          tabIndex="0"
+        >
+          <FontAwesomeIcon icon={collapsed ? faAngleRight : faAngleDown} />
+          <FontAwesomeIcon icon={faFile} />
+          {rightDataset.resource.name}
+        </span>
+        <span>{matches ? `${matches.length} matching ids` : 'loading'}</span>
+        <span>
+          <Link to={`/dataset/${rightDataset.resource.id}`}>View dataset</Link>
+        </span>
+        <button
+          type="button"
+          onClick={() =>
+            collection.datasets.includes(rightDataset.resource.id)
+              ? removeFromCollection(rightDataset.resource.id)
+              : addToCollection(rightDataset.resource.id)
+          }
+        >
+          {collection.datasets.includes(rightDataset.resource.id)
+            ? 'Remove dataset from collection'
+            : 'Add dataset to collection'}
+        </button>
+      </div>
+      {!collapsed &&
+        (matches ? (
+          <div className="join-column-unique">
+            <h3>COMMON IDS</h3>
+            <ul>
+              {matches.slice(0, 10).map((uid) => (
+                <li key={uid}>
+                  <div className="bar" />
+                  <FontAwesomeIcon icon={faTable} />
+                  <span>{uid}</span>
+                </li>
+              ))}
+            </ul>
+            <div>and {matches.length - 10} more</div>
+          </div>
+        ) : (
+          <h2>Loading</h2>
+        ))}
+    </div>
+  );
 }
