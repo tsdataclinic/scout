f99b78cb8dd4f151f4dda8b23928c3b7c8656daf
Stuart Lynn <stuart.lynn@gmail.com>
2020-03-03 16:33:26 -0500

    missed collection page

---
+++ src/layout/CollectionsPage/CollectionsPage.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useCollections } from '../../hooks/collections';
+import CollectionCard from '../../components/CollectionCard/CollectionCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLayerGroup } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import './CollectionsPage.scss';
+import { constructCollectionLink } from '../../utils/formatters';
+
+export default function CollectionsPage() {
+    const [{ collections }] = useCollections();
+
+    return (
+        <div className="collections-page">
+            <div className="collections-header">
+                <h2>My Collections</h2>
+                <p>{collections.length} Collections</p>
+            </div>
+
+            {collections.length == 0 && (
+                <div className="no-collections">
+                    <div className="collection-icon">
+                        <FontAwesomeIcon icon={faLayerGroup} />
+                    </div>
+                    <h3>No Collections found</h3>
+                    <p>
+                        Select 'Add to Collection' on datasets to begin creating
+                        local connections
+                    </p>
+                    <div className="what-are-collections">
+                        <h3>What are Collections?</h3>
+                        <p>
+                            Collections allow you to group, save and share
+                            similar datasets. Collections are saved locally - to
+                            share your Collection use the share url. If a
+                            dataset updates on the open data portal, it will
+                            also update within your collection
+                        </p>
+                    </div>
+                </div>
+            )}
+            {collections.length > 0 && (
+                <div className="collections-list">
+                    <ul>
+                        {collections.map((collection) => (
+                            <li>
+                                <Link to={constructCollectionLink(collection)}>
+                                    <CollectionCard collection={collection} />
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+}
