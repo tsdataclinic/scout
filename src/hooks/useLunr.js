3485240732fddda2b46a538d4bf54741508b73ff
Stuart Lynn <stuart.lynn@gmail.com>
2020-04-20 15:46:59 -0400

    Updating the hook to catch errors and to recompute search on index change

--- src/hooks/useLunr.js
+++ src/hooks/useLunr.js
@@ -2,31 +2,36 @@ import { useState, useRef, useEffect } from 'react';
 import lunr from 'lunr';
 
 const parameterisedPlugin = function(builder, fields) {
-  fields.forEach(function(field) {
-    builder.field(field);
-  });
+    fields.forEach(function(field) {
+        builder.field(field);
+    });
 };
 function generateIndex(options, documents) {
-  return lunr(function() {
-    this.use(parameterisedPlugin, options.fields);
+    return lunr(function() {
+        this.use(parameterisedPlugin, options.fields);
 
-    documents.forEach(function(doc) {
-      this.add(doc);
-    }, this);
-  });
+        documents.forEach(function(doc) {
+            this.add(doc);
+        }, this);
+    });
 }
 
 export default function useLunr({ query, documents, options }) {
-  const index = useRef(null);
-  const [results, setResults] = useState([]);
-  useEffect(() => {
-    index.current = generateIndex(options, documents);
-  }, [documents, options]);
+    const index = useRef(null);
+    const [results, setResults] = useState([]);
 
-  useEffect(() => {
-    if (index.current) {
-      setResults(index.current.search(query));
-    }
-  }, [query]);
-  return results;
+    useEffect(() => {
+        index.current = generateIndex(options, documents);
+    }, [documents.map((d) => d.id).join('_')]);
+
+    useEffect(() => {
+        if (index.current) {
+            try {
+                setResults(index.current.search(query));
+            } catch {
+                console.log('ignoring bad query format');
+            }
+        }
+    }, [query, index.current]);
+    return results;
 }
