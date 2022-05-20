3477e6b7e87af49a67818da8f72d00ceb6f92d6b
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-05-20 03:05:26 -0400

    Call typeorm directly in aws script

--- scripts/aws/prepare.sh
+++ scripts/aws/prepare.sh
@@ -6,7 +6,7 @@ cp /var/www/scout-server.env .env
 # Run the db migration
 # TODO: use an actual migration script, which is what TypeORM recommends,
 # instead of using the sync command
-yarn sync-schema
+typeorm schema:sync
 
 # prepare the frontend
 cd /srv/scout/packages/frontend
