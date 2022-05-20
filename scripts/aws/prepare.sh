b0605d688d17609f5fa238d307a47a31f1b22703
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-05-20 03:47:32 -0400

    Updated build scripts

--- scripts/aws/prepare.sh
+++ scripts/aws/prepare.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-# prepare the API server
+# install all dependencies
+cd /srv/scout
+yarn install
+
+# build the API server
 cd /srv/scout/packages/server
 cp /var/www/scout-server.env .env
+yarn build
 
 # Run the db migration
 # TODO: use an actual migration script, which is what TypeORM recommends,
 # instead of using the sync command
-typeorm schema:sync
+yarn sync-schema
 
-# prepare the frontend
+# build the frontend
 cd /srv/scout/packages/frontend
 cp /var/www/scout-client.env .env
+yarn build
