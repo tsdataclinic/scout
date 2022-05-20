9a2bf6b265395f6f52f519c8da556a07b5b69222
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-05-20 02:29:08 -0400

    add silent flag

--- scripts/aws/cleanup.sh
+++ scripts/aws/cleanup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # Cleanup things from the previous deployment
-source ~/.bash_profile
-echo $HOME
-echo $PATH
-which npm
-which pm2
-/usr/bin/pm2 stop all
-/usr/bin/pm2 delete all
+pm2 stop all --silent
+pm2 delete all --silent
