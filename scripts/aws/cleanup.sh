d68a1ba5a6837153859c9416f92c8a2803d99e39
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-05-20 02:15:34 -0400

    add debug statements

--- scripts/aws/cleanup.sh
+++ scripts/aws/cleanup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Cleanup things from the previous deployment
 source ~/.bash_profile
-echo "Trying pm2"
+echo $HOME
+echo $PATH
+which npm
+which pm2
 /usr/bin/pm2 stop all
 /usr/bin/pm2 delete all
