e654fbc78faf091e8d8e0f46bdcd76d69d5cf840
Indraneel Purohit <indraneel.purohit@twosigma.com>
2022-07-18 16:24:26 -0400

    URL encode collection name (#294)
    * URL encode collection name
    
    use `encodeURIComponent()` on collection name to prevent `/`s from breaking the `<Route>`
    
    * extract inline encodeUri call

--- packages/frontend/src/layout/CollectionPage/CollectionPage.jsx
+++ packages/frontend/src/layout/CollectionPage/CollectionPage.jsx
@@ -26,7 +26,8 @@ const EMPTY_COLLECTION = {
 function getShareableURLForUnauthenticated(collectionName, datasetIds) {
   const urlOrigin = window.location.origin;
   const datasetIdsStr = datasetIds.join(',');
-  return `${urlOrigin}/collection/${collectionName}/${datasetIdsStr}`;
+  const uriEncodedCollectionName = encodeURIComponent(collectionName);
+  return `${urlOrigin}/collection/${uriEncodedCollectionName}/${datasetIdsStr}`;
 }
 
 // TODO: $CollectionsManagement - make shareable URLs for authenticated users work
