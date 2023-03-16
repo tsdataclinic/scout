2fa6dd79456b5cdec0182c17ef33f0f723c242b8
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2023-03-16 10:21:30 -0400

    Added viz picker

--- packages/frontend/src/layout/DatasetPage/VisualizationExplorer/index.tsx
+++ packages/frontend/src/layout/DatasetPage/VisualizationExplorer/index.tsx
@@ -1,9 +1,11 @@
+import * as React from 'react';
 import { useQuery } from 'react-query';
 import { getFullDataset } from '../../../utils/socrata';
 import socrataResultsToDataframe from './socrataResultsToDataframe';
 import type { Dataset, ParsedCSVResults } from './socrataResultsToDataframe';
 import * as DX from '../../../components/common/DataExplorer';
 import Select, { type SelectOption } from '../../../components/ui/Select';
+import assertUnreachable from '../../../utils/assertUnreachable';
 
 type Props = { dataset: Dataset };
 
@@ -27,6 +29,8 @@ const VIZ_OPTIONS: ReadonlyArray<SelectOption<DX.VizType>> = [
 ];
 
 export default function VisualizationExplorer({ dataset }: Props): JSX.Element {
+  const [selectedViz, setSelectedViz] = React.useState<DX.VizType>('table');
+
   const { data: dataframe, isLoading } = useQuery(
     ['datasetResults', dataset?.id],
     async (): Promise<DX.Dataframe> => {
@@ -47,13 +51,37 @@ export default function VisualizationExplorer({ dataset }: Props): JSX.Element {
     },
   );
 
+  const renderVisualization = (): JSX.Element => {
+    if (dataframe) {
+      switch (selectedViz) {
+        case 'table':
+          return <DX.Table dataframe={dataframe} />;
+        case 'histogram':
+          return <div>histogram</div>;
+        case 'scatterplot':
+          return <div>scatterplot</div>;
+        case 'map':
+          return <div>map</div>;
+        default:
+          return assertUnreachable(selectedViz);
+      }
+    }
+
+    return (
+      <p>{isLoading ? 'Loading...' : 'An error occurred loading the data'}</p>
+    );
+  };
+
   return (
     <div>
-      {isLoading ? 'Loading...' : null}
       {dataframe ? (
         <div className="space-y-4">
-          <Select options={VIZ_OPTIONS} defaultValue="table" />
-          <DX.Table dataframe={dataframe} />
+          <Select
+            options={VIZ_OPTIONS}
+            value={selectedViz}
+            onChange={setSelectedViz}
+          />
+          {renderVisualization()}
         </div>
       ) : null}
     </div>
