eb7708cf0f48cfa84e5d6507adc1ee655d55f471
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2023-03-17 16:11:07 -0400

    Points are now plottable on map with tooltips

--- packages/frontend/src/components/common/DataExplorer/MapViz.tsx
+++ packages/frontend/src/components/common/DataExplorer/MapViz.tsx
@@ -19,37 +19,80 @@ function PointTooltip({
 }): JSX.Element {
   const fieldsMap = new Map(fields.map(field => [field.id, field]));
   return (
-    <dl className="space-y-1">
-      {R.pipe(
-        dataRow,
-        R.keys,
-        R.sortBy([key => key, 'asc']),
-        R.map(fieldId => (
-          <div key={fieldId} className="flex justify-between space-x-4">
-            <dt>{fieldsMap.get(fieldId)?.displayName ?? fieldId}</dt>
-            <dd>{dataRow[fieldId] ?? 'null'}</dd>
-          </div>
-        )),
-      )}
-    </dl>
+    <div
+      style={{
+        height: 396,
+        margin: '-10px -10px',
+      }}
+      className="overflow-auto p-2 px-3"
+    >
+      <dl className="space-y-1">
+        {R.pipe(
+          dataRow,
+          R.keys,
+          R.sortBy([key => key, 'asc']),
+          R.map(fieldId => (
+            <div key={fieldId} className="flex justify-between space-x-4">
+              <dt>{fieldsMap.get(fieldId)?.displayName ?? fieldId}</dt>
+              <dd>{dataRow[fieldId] ?? 'null'}</dd>
+            </div>
+          )),
+        )}
+      </dl>
+    </div>
   );
 }
+
+const LATITUDE_FIELD_POSSIBILITIES = ['latitude', 'lat', 'y'];
+const LONGITUDE_FIELD_POSSIBILITIES = ['longitude', 'long', 'x'];
 
 export function MapViz({ dataframe }: Props): JSX.Element {
   const { data, fields } = dataframe;
   const mapRef = React.useRef<mapboxgl.Map | null>(null);
   const tooltipRef = React.useRef(
-    new mapboxgl.Popup({ offset: 15, maxWidth: '400px', closeButton: false }),
+    new mapboxgl.Popup({
+      offset: 15,
+      maxWidth: '400px',
+      closeButton: false,
+    }),
   );
 
-  // TODO: first check if anything seems like it might be a geolocation
+  const [latField, longField] = React.useMemo(() => {
+    // first, keep only fields that are text or number.
+    const possibleFields = fields.filter(
+      field => field.type === 'text' || field.type === 'number',
+    );
+
+    // find latitude field
+    const lat = possibleFields.find(field =>
+      LATITUDE_FIELD_POSSIBILITIES.some(
+        possibleId => possibleId === field.id.toLowerCase(),
+      ),
+    );
+
+    // find longitude field
+    const lng = possibleFields.find(field =>
+      LONGITUDE_FIELD_POSSIBILITIES.some(
+        possibleId => possibleId === field.id.toLowerCase(),
+      ),
+    );
+    return [lat, lng];
+  }, [fields]);
+
   const geojson: GeoJSONSourceRaw = React.useMemo(() => {
-    const slicedData = data.slice(0, 100);
+    if (latField && longField) {
+      return {
+        type: 'geojson',
+        data: GeoJSON.parse(data, { Point: [latField.id, longField.id] }),
+      };
+    }
+
+    // we couldn't find a lat/long field so return an empty geojson
     return {
       type: 'geojson',
-      data: GeoJSON.parse(slicedData, { Point: ['latitude', 'longitude'] }),
+      data: { type: 'FeatureCollection', features: [] },
     };
-  }, [data]);
+  }, [data, latField, longField]);
 
   // set up the map
   React.useEffect(() => {
@@ -68,7 +111,6 @@ export function MapViz({ dataframe }: Props): JSX.Element {
 
       map.on('load', () => {
         // add the points as a GeoJSON source
-        console.log(geojson);
         map.addSource('points', geojson);
 
         // add a circle layer
@@ -115,5 +157,15 @@ export function MapViz({ dataframe }: Props): JSX.Element {
     }
   }, [geojson, fields]);
 
-  return <div id="map" className="h-96 w-full sm:h-[90%]" />;
+  return (
+    <>
+      {latField === undefined || longField === undefined ? (
+        <p className="text-red-700 text-base">
+          No points could be mapped because we could not find a latitude or
+          longitude column for this dataset.
+        </p>
+      ) : null}
+      <div id="map" className="h-96 w-full sm:h-[90%]" />;
+    </>
+  );
 }
